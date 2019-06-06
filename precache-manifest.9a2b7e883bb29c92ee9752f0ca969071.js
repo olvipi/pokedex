728ad8004d53877199d730e49c6f7eff14dd5552
olvipi <ovpischuk@gmail.com>
2019-06-06 14:29:59 +0300

    Updates

---
+++ precache-manifest.9a2b7e883bb29c92ee9752f0ca969071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ca97bf1e9e81e6c42b50e071bbe427",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "340d2e32d646e36bdfe8",
+    "url": "/pokedex/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b1615c3ac62a3ba9ea25",
+    "url": "/pokedex/static/css/main.58266a16.chunk.css"
+  },
+  {
+    "revision": "340d2e32d646e36bdfe8",
+    "url": "/pokedex/static/js/2.7e9d2fc7.chunk.js"
+  },
+  {
+    "revision": "b1615c3ac62a3ba9ea25",
+    "url": "/pokedex/static/js/main.161fca00.chunk.js"
+  },
+  {
+    "revision": "c92f8f4bce56a57764ce",
+    "url": "/pokedex/static/js/runtime~main.a0407843.js"
+  }
+]);
