09069d2e96c2be37a15cc08ea683512f1a8e0205
olvipi <ovpischuk@gmail.com>
2019-06-08 01:33:45 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pokedex/precache-manifest.67a0c7479c4ff7928aea026ce870e4fc.js"
+  "/pokedex/precache-manifest.edd196b82dbf8b6d9a5f5cddf447a530.js"
 );
 
 self.addEventListener('message', (event) => {
